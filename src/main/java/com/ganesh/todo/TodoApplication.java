362b66403319f62deae3e4337c694d342e9c4313
ganesh11499 <ganeshramesh11499@gmail.com>
2026-06-22 09:23:48 +0530

    deployement changes

--- src/main/java/com/ganesh/todo/TodoApplication.java
+++ src/main/java/com/ganesh/todo/TodoApplication.java
@@ -3,11 +3,15 @@ package com.ganesh.todo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class TodoApplication {
 
     public static void main(String[] args) {
+
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+
         SpringApplication.run(TodoApplication.class, args);
     }
-
 }
